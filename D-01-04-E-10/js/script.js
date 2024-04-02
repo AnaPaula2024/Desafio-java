151c2f08769ae83270672e83529ddea5f29c2366
ANA PAULA PEREIRA VIANA <ana.ppviana@senacsp.edu.br>
2024-04-02 15:45:31 -0300

    Atualização

--- D-01-04-E-10/js/script.js
+++ D-01-04-E-10/js/script.js
@@ -1,1 +1,11 @@
 // 10. Escreva um programa em JavaScript que solicite um número ao usuário e exiba todos os números ímpares entre 1 e o número informado. Utilize uma estrutura de repetição while ou for para verificar se cada número entre 1 e o número informado é ímpar, e exibir os números ímpares encontrados no console.
+
+
+let = (prompt("Digite um número"));
+
+let n1 = 1;
+let ni = "";
+
+for (let n1= 1; ni <= "";) {
+    console.log(`Número ímpar: ${n1}`);
+}
