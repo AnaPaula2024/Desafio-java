674e25aecd86a574353b427e448d1ff7dd779bd6
ANA PAULA PEREIRA VIANA <ana.ppviana@senacsp.edu.br>
2024-04-02 14:52:03 -0300

    Desafio-js

---
+++ D-01-04/js/script.js
@@ -0,0 +1,6 @@
+// Exercício 01
+
+// 1. Escreva um programa em JavaScript que solicite dois números ao usuário e exiba o maior número no console. Utilize uma estrutura de decisão if para comparar os números e exibir o maior número no console.
+
+var numero1 = parseFloat(prompt("7"));
+var numero2 = parseFloat(prompt("28"));
